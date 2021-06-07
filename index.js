2ea4daa832c8d3b46323326c73f0a746b0ca626c
c-noblet <56131432+c-noblet@users.noreply.github.com>
2021-06-07 13:02:22 +0200

    Code comments added

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const cache = [];
 
+// Loop until the game has started
 const gameStartChecker = setInterval(function () {
   const gameFieldCards = document.querySelectorAll('.game-field-card, .game-selection-card-image');
   const gameField = document.querySelector('#game-field-player-hand');
+  
+  // If the game field is present
   if (gameField) {
+    
+    // Add mouse hover listener on cards
     gameFieldCards.forEach((item) => {
       item.addEventListener('mouseover', function () {
         const cardName = document.querySelector('#card-name').textContent;
+        
+        // Check if the card is in the cache
         cardInCache = false;
         cache.forEach((card) => {
           if (card.name === cardName) {
+            
+            // Display the french text in the description area
             document.querySelector('#card-description').innerHTML = card.text;
             cardInCache = true;
           }
         });
+        
+        // If the card is not in the cache, fetch it and add it to the cache 
         if (!cardInCache) {
           fetch('https://yugipedia.com/wiki/'+ cardName.replace(' ', '_'))
           .then((response) => response.text())
@@ -24,12 +35,16 @@ const gameStartChecker = setInterval(function () {
               text: text
             }
             cache.push(card);
+            
+            // Display the french text in the description area
             document.querySelector('#card-description').innerHTML = card.text;
           })
           .catch((err) => console.error(err));
         }
       });
     });
+    
+    // Stop the loop
     clearInterval(gameStartChecker);
   }
 }, 1000);
